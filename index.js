85df863394dbd1f2672179b9c1b64d7287a26474
Peolunics <143931246+Peolunics@users.noreply.github.com>
2025-01-06 16:56:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSource = "/images/"+randomDiceImage;
+
+var images1 = document.querySelectorAll("img")[0];
+images1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice"+randomNumber2+".png";
+var randomImageSource = "/images/"+randomDiceImage;
+
+var images1 = document.querySelectorAll("img")[1];
+images1.setAttribute("src",randomImageSource);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 is the winner";
+  }
+  else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 is the winner";
+  }
+  else{
+    document.querySelector("h1").innerHTML="Draw";
+  }
